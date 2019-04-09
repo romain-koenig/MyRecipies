b00e2dbe85e62b858068dabf85fc00846aecf2f0
RKO <romain.koenig@fiducial.net>
2019-04-09 07:29:23 +0200

    Maquette SLS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyRecipies';
+  title = 'Gestion du SLS';
+  modificationHidden = true;
+  finalHidden = true;
+
+  Nom = "LAFORET";
+  Prenom = "Albert";
+  DateDeNaissance = new Date(1976, 3, 1);
+
+  NomModif = "LAFORET";
+  PrenomModif = "Albert";
+  DateDeNaissanceModif = new Date(1976, 3, 1);
+
+  onModifier() {
+    this.modificationHidden = !this.modificationHidden;
+    
+  }
+  onValider() {
+    this.modificationHidden = true;
+
+    if (this.Nom != this.NomModif || 
+      this.Prenom != this.PrenomModif ||
+      this.DateDeNaissance != this.DateDeNaissanceModif) {
+        this.finalHidden = false;
+      }
+      else {
+        this.finalHidden = true;
+      }
+  }
+
+  onReinit() {
+
+    this.NomModif = this.Nom;
+    this.PrenomModif = this.Prenom;
+    this.DateDeNaissanceModif = this.DateDeNaissance;
+    
+    this.finalHidden = true;
+    this.modificationHidden = true;
+  }
 }
